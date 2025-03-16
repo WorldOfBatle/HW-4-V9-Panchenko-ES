2c15b14c4cb28dde3ddece11a5b9364430bc50a4
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-16 19:36:42 +0300

    Инициализация b1, b2

--- HW-4/main.cpp
+++ HW-4/main.cpp
@@ -13,5 +13,20 @@ int main()
         return 0; // завершаем программу
     }
 
+    long long b1 = -10; // b_1
+    long long b2 = 2;   // b_2
+
+    // Если n >= 1, выводим b1
+    if (n >= 1)
+    {
+        std::cout << b1 << " ";
+    }
+
+    // Если n >= 2, выводим b2
+    if (n >= 2)
+    {
+        std::cout << b2 << " ";
+    }
+
     return 0;
 }
