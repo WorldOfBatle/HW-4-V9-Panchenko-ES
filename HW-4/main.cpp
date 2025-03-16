0797ea08f28be00392bf039e68a970d4d7cabbf8
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-16 19:39:59 +0300

    Реализован цикл для b3..bn Финал

--- HW-4/main.cpp
+++ HW-4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath> // для std::llabs
 
 // Заготовка
 int main()
@@ -28,5 +29,20 @@ int main()
         std::cout << b2 << " ";
     }
 
+    // Вычисляем b3..bn, если n > 2
+    for (int i = 3; i <= n; i++)
+    {
+        // b_n = |b_{n-2}| - 6 * b_{n-1}
+        long long next = std::llabs(b1) - 6LL * b2;
+
+        // Выводим очередной элемент
+        std::cout << next << " ";
+
+        // Сдвигаем: теперь b1 = b2, b2 = next
+        b1 = b2;
+        b2 = next;
+    }
+
+    std::cout << std::endl;
     return 0;
 }
